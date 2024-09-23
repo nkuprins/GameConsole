9fecfc48b93f7a0d134eae150fb116e8e785f64c
Nikita Kuprins <nikita.kuprins@student.science.ru.nl>
2024-09-23 09:51:08 +0200

    The comments are removed

--- project/project.ino
+++ project/project.ino
@@ -3,36 +3,9 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 
-/* This driver uses the Adafruit unified sensor library (Adafruit_Sensor),
-   which provides a common 'type' for sensor data and some helper functions.
-
-   To use this driver you will also need to download the Adafruit_Sensor
-   library and include it in your libraries folder.
-
-   You should also assign a unique ID to this sensor for use with
-   the Adafruit Sensor API so that you can identify this particular
-   sensor in any data logs, etc.  To assign a unique ID, simply
-   provide an appropriate value in the constructor below (12345
-   is used by default in this example).
-
-   Connections
-   ===========
-   Connect SCL to SCL pin (analog 5 on Arduino UNO)
-   Connect SDA to SDA pin (analog 4 on Arduino UNO)
-   Connect VDD to 3-5V DC (depending on your board's logic level)
-   Connect GROUND to common ground
-
-   History
-   =======
-   2015/MAR/03  - First release (KTOWN)
-   2015/AUG/27  - Added calibration and system status helpers
-*/
-
 /* Set the delay between fresh samples */
 #define BNO055_SAMPLERATE_DELAY_MS (100)
 
-// Check I2C device address and correct line below (by default address is 0x29 or 0x28)
-//                                   id, address
 Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x28, &Wire);
 
 /**************************************************************************/
@@ -113,51 +86,35 @@ void displayCalStatus(void)
   Serial.print(mag, DEC);
 }
 
-/**************************************************************************/
-/*
-    Arduino setup function (automatically called at startup)
-*/
-/**************************************************************************/
 void setup(void)
 {
   Serial.begin(115200);
 
-  while (!Serial) delay(10);  // wait for serial port to open!
+  while (!Serial)
+    delay(10);
 
-  Serial.println("Orientation Sensor Test"); Serial.println("");
+  Serial.println("Orientation Sensor Test"); 
+  Serial.println("");
 
-  /* Initialise the sensor */
   if(!bno.begin())
   {
-    /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
     while(1);
   }
 
   delay(1000);
 
-  /* Display some basic information on this sensor */
   displaySensorDetails();
-
-  /* Optional: Display current status */
   displaySensorStatus();
 
   bno.setExtCrystalUse(true);
 }
 
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
 void loop(void)
 {
-  /* Get a new sensor event */
   sensors_event_t event;
   bno.getEvent(&event);
 
-  /* Display the floating point data */
   Serial.print("X: ");
   Serial.print(event.orientation.x, 4);
   Serial.print("\tY: ");
@@ -165,15 +122,12 @@ void loop(void)
   Serial.print("\tZ: ");
   Serial.print(event.orientation.z, 4);
 
-  /* Optional: Display calibration status */
   displayCalStatus();
 
   /* Optional: Display sensor status (debug only) */
   //displaySensorStatus();
 
-  /* New line for the next sample */
   Serial.println("");
 
-  /* Wait the specified delay before requesting nex data */
   delay(BNO055_SAMPLERATE_DELAY_MS);
 }
