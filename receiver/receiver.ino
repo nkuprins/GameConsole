5d23e8dfead3db26c2d14041b0747b29745afb78
Nikita Kuprins <nikita.kuprins@student.science.ru.nl>
2024-09-26 08:55:47 +0200

    add auxiliary functions for wifi and handling connection

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -1,17 +1,52 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+#define LISTEN_PORT 80
+ 
+const char* ssid = "NDL_24G"; //name of local WiFi network in the NDL
+const char* password = "RT-AC66U"; 
+
+MDNSResponder mdns;
+ESP8266WebServer server(LISTEN_PORT);
+
+bool ledOn = false;
+
+void connectToWifi() {
+  WiFi.begin(ssid, password); // Start Wi-Fi connection to specified access point
+  Serial.println("Start connecting.");
+  while (WiFi.status() != WL_CONNECTED) { // Wait until we are connected
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.print("Connected to ");
+  Serial.print(ssid);
+  Serial.print(", IP address: ");
+  Serial.println(WiFi.localIP()); // Print local IP to Serial Monitor
+
+  if(mdns.begin("esp8266", WiFi.localIP())) {
+    Serial.println("MDNS responder started");
+  }
+}
+
+void hanleConnection() {
+  server.on("/", [](){
+    Serial.println("RECEIVED 382u8932784984902384902");
+  });
+}
 
 void setup() {
-   pinMode(LED_BUILTIN, OUTPUT); // initialize pin as output
-   Serial.begin(9600); // initialize serial communication
-   while (!Serial) {
-     Serial.println("Waiting");
-     delay(100);
-   }
-   Serial.println("Hello, world!"); // print "Hello, world!" to the serial monitor
+  Serial.begin(115200);
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, ledOn); 
+  
+  connectToWifi();
+  hanleConnection();
+  server.begin();
+  Serial.println("HTTP server started");
 }
 void loop() {
-   digitalWrite(LED_BUILTIN, HIGH); // switch LED on (HIGH voltage)
-   delay(1000); // wait 1000 ms, i.e. 1 s
-   digitalWrite(LED_BUILTIN, LOW); // switch LED off (LOW voltage)
-   delay(1000); // wait a second
-   Serial.println("Good morning.");
+  server.handleClient(); //Handle all website logic, this should be run every loop!
+  mdns.update();
 }
